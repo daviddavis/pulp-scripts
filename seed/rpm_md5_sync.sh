88fb54216ea30b18a6629b7a487f1db15c756b89
David Davis <daviddavis@redhat.com>
2021-02-17 08:26:53 -0500

    Added md5 repo seed script

---
+++ seed/rpm_md5_sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+
+repo_name="md5-repo$RANDOM"
+remote_name="md5-remote$RANDOM"
+url="https://fixtures.pulpproject.org/rpm-with-md5/"
+
+pulp rpm remote create --name $remote_name --url $url
+pulp rpm repository create --name $repo_name --remote $remote_name
+pulp rpm repository sync --name $repo_name
+pulp rpm publication create --repository $repo_name
+
+echo "Created rpm repo $repo_name"
